7953731532aa47f6547e49fb6adbb41ee5ea667a
frnkly <frank@frnk.ca>
2015-11-08 21:30:59 -0500

    Added meta tags

--- public/index.php
+++ public/index.php
@@ -9,6 +9,17 @@ require '../lib/asset.php';
 <head>
     <meta charset="utf-8">
     <title>A Collection of Cultures - Di Nkɔmɔ</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="author" content="Francis Amankrah">
+	<meta name="description" content="Di Nkɔmɔ: A Collection of Cultures">
+	<meta name="keywords" content="Di Nkɔmɔ, Twi, Akan, Ghana">
+	<meta name="robots" content="index, follow">
+
+    <meta property="og:title" content="Di Nkɔmɔ: A Collection of Cultures">
+	<meta property="og:desc" content="Di Nkɔmɔ: A Collection of Cultures">
+	<meta property="og:type" content="website">
+    
     <link rel="stylesheet" href="<?= Asset::rev('learn.css') ?>">
 </head>
 <body>

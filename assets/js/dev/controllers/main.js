c8fe0f77ef43728e5281964c8785d3079057899b
frnkly <frank@frnk.ca>
2015-11-09 09:38:04 -0500

    Working on nav for mobile

--- assets/js/dev/controllers/main.js
+++ assets/js/dev/controllers/main.js
@@ -3,14 +3,20 @@
  */
 angular.module('nkomo.controllers')
 
-.controller('MainController', ['$scope', '$sessionStorage', '$localStorage', 'assetVersion',
-    function($scope, $sessionStorage, $localStorage, assetVersion) {
+.controller('MainController', ['$scope', '$sessionStorage', '$localStorage', 'Rover', 'assetVersion',
+    function($scope, $sessionStorage, $localStorage, Rover, assetVersion) {
 
         $scope.assetVersion = assetVersion;
 
         // Globally available language and definition objects.
         $sessionStorage.languages = $sessionStorage.languages || {};
         $sessionStorage.definitions = $sessionStorage.definitions || {};
+
+        // Toggles navigation links.
+        $scope.toggleNav = function() {
+            Rover.debug('toggling nav');
+            $('aside .hidden-xs').show();
+        };
 
     }
 ]);
